dee7317ce605d886607909a2e8dfef1e70392821
Thomsch <thms.sch@gmail.com>
2018-04-06 18:27:49 -0400

    Added constants

--- src/main/java/ch/thomsch/ToyExample.java
+++ src/main/java/ch/thomsch/ToyExample.java
@@ -1,13 +1,18 @@
 package ch.thomsch;
 
+import java.io.File;
+
 /**
  * @author TSC
  */
 public class ToyExample {
+
+    public static final String REVISION_FILE = "src/main/resources/toy-refactorings.csv";
+    public static final String REPOSITORY = "../refactoring-toy-example";
+    public static final String RESULTS_FILE = "./toy-refactorings-metrics.csv";
+
     public static void main(String[] args) {
         new MetricHistory(new Collector(), new VersionControl(), new Reporter())
-                .collect("src/main/resources/toy-refactorings.csv",
-                        "../refactoring-toy-example",
-                        "./toy-refactorings-metrics.csv");
+                .collect(REVISION_FILE, REPOSITORY, RESULTS_FILE);
     }
 }
