cdf5b6a96552b7b5bd237cf22779519cb18df397
Thomsch <thms.sch@gmail.com>
2018-04-19 12:51:31 -0400

    Added runner for projects

--- src/test/java/ch/thomsch/example/ProjectsData.java
+++ src/test/java/ch/thomsch/example/ProjectsData.java
@@ -1,5 +1,8 @@
 package ch.thomsch.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 
 import ch.thomsch.Collector;
@@ -12,16 +15,41 @@ import ch.thomsch.versioncontrol.GitRepository;
  * @author TSC
  */
 public class ProjectsData {
-    private static final String REVISION_FILE = "../mined-repositories/dagger.csv";
-    private static final String REPOSITORY = "../mined-repositories/dagger";
-    private static final String RESULTS_FILE = "../mined-repositories/results/dagger-metrics.csv";
+    private static final String BASE = "../mined-repositories/";
 
-    public static void main(String[] args) throws IOException {
+    private static final Logger logger = LoggerFactory.getLogger(ProjectsData.class);
+
+    public static void main(String[] args) {
         final MetricHistory metricHistory = new MetricHistory(
                 new Collector(),
                 new Reporter(),
                 new ModifiedRMinerReader());
 
-        metricHistory.collect(REVISION_FILE, GitRepository.get(REPOSITORY), RESULTS_FILE);
+
+//        collectProject(metricHistory, "dagger");
+        collectProject(metricHistory, "dagger2");
+        collectProject(metricHistory, "jena");
+        collectProject(metricHistory, "jfreechart");
+    }
+
+    private static void collectProject(MetricHistory metricHistory, String projectName) {
+        try {
+            metricHistory.collect(getRevisionsLocation(projectName), getRepository(projectName), getOutputFile
+                    (projectName));
+        } catch (IOException e) {
+            logger.error("Repository not found for project {}", projectName, e);
+        }
+    }
+
+    private static String getOutputFile(String projectName) {
+        return BASE + "results/" + projectName + "-metrics.csv";
+    }
+
+    private static GitRepository getRepository(String projectName) throws IOException {
+        return GitRepository.get(BASE + projectName);
+    }
+
+    private static String getRevisionsLocation(String projectName) {
+        return BASE + projectName + ".csv";
     }
 }
