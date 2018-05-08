739e164babbf587874aeaef9265636297c473fe0
Thomsch <thms.sch@gmail.com>
2018-05-08 17:10:51 -0400

    Added 'convert' action to command line

---
+++ src/main/java/ch/thomsch/converter/SourceMeterConverter.java
@@ -0,0 +1,16 @@
+package ch.thomsch.converter;
+
+/**
+ * @author Thomsch
+ */
+public class SourceMeterConverter {
+
+    /**
+     * Convert results in the SourceMeter format to the raw format.
+     *
+     * @param inputPath  the path of the folder containing SourceMeter's results
+     * @param outputPath the path of the file that will contain the results
+     */
+    public static void convertClasses(String inputPath, String outputPath) {
+    }
+}
