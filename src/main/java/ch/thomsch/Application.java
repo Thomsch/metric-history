b814dd157d0a088e89415ae3fdf9b586cf59ec79
Thomas Schweizer <thomas.schweizer@umontreal.ca>
2018-09-21 13:24:36 -0400

    Extracted commands from Application

--- src/main/java/ch/thomsch/Application.java
+++ src/main/java/ch/thomsch/Application.java
@@ -8,7 +8,11 @@ import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 import ch.thomsch.converter.SourceMeterConverter;
 import ch.thomsch.csv.Stores;
@@ -25,7 +29,17 @@ import ch.thomsch.versioncontrol.GitRepository;
  * Entry point for the application.
  */
 public final class Application {
-    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+    private final Map<String, Command> commands = new HashMap<>();
+    private final Command.Help help;
+
+    Application() {
+        help = new Command.Help(commands.values());
+        addCommand(new Command.Collect());
+        addCommand(new Command.Convert());
+        addCommand(new Command.Ancestry());
+        addCommand(new Command.Difference());
+        addCommand(new Command.Mongo());
+    }
 
     /**
      * Parse and execute from the command line.
@@ -39,243 +53,49 @@ public final class Application {
             printHelp();
             return;
         }
-
-        try {
-            processSubCommand(args);
-        } catch (IOException e) {
-            logger.error("An I/O error occurred while reading or writing a file", e);
-        }
-    }
-
-    private void processSubCommand(String[] args) throws IOException {
-        switch (args[0]) {
-            case "collect":
-                processCollectCommand(args);
-                break;
-
-            case "ancestry":
-                processAncestryCommand(args);
-                break;
-
-            case "convert":
-                processConvertCommand(args);
-                break;
-
-            case "diff":
-                processDiffCommand(args);
-                break;
-
-            case "mongo":
-                processMongoCommand(args);
-                break;
-
-            default:
-                System.out.println("Unknown command '" + args[0] + "'. Verify the spelling and make sure your command" +
-                        " is in lowercase.");
-                System.out.println();
-                printHelp();
-                break;
-        }
-    }
-
-    /**
-     * Prints usage instructions in the terminal
-     */
-    private void printHelp() {
-        System.out.println("Usage: metric-history <command> <parameters>...");
-        System.out.println("or metric-history <command> to learn more.");
-        System.out.println();
-        System.out.println("Where <command> = collect|ancestry|convert|diff|mongo");
-    }
-
-    public void processCollectCommand(String[] args) {
-        if (args.length == 1) {
-            System.out.println("Usage: metric-history collect <revision file> <executable path> <project path> " +
-                    "<repository path> <output dir> <project name>");
-            System.out.println();
-            System.out.println("<revision file>     is the path to the file containing the revision to analyse.");
-            System.out.println("<executable path>   is the path to the executable to collect metrics.");
-            System.out.println("<project path>      is the path to the folder containing the source code or the " +
-                    "project.");
-            System.out.println("<repository path>   is the path to the folder containing .git folder. It can also be " +
-                    "set to 'same' if it's the same as <project path>.");
-            System.out.println("<output dir>        is the path to the folder where the results should be extracted.");
-            System.out.println("<project name>      is the name of the project.");
+        final String commandName = args[0];
+        final Command command = commands.get(commandName);
+        if(command == null) {
+            printUnknownCommand(commandName);
             return;
         }
 
-        atLeast(7, args);
+        final String[] parameters = Arrays.copyOfRange(args, 1, args.length);
+        if(parameters.length == 0) {
+            command.printUsage();
+            return;
+        }
 
-        String revisionFile = normalizePath(args[1]);
-        String executable = normalizePath(args[2]);
-        String project = normalizePath(args[3]);
-
-        String repository = args[4];
-        if (repository.equalsIgnoreCase("same")) {
-            repository = project;
+        if (command.parse(parameters)) {
+            command.execute();
         } else {
-            repository = normalizePath(repository);
-        }
-
-        String executableOutput = normalizePath(args[5]);
-        String projectName = args[6];
-
-        try {
-            Collector collector = new SourceMeter(executable, executableOutput, projectName, project);
-            MetricHistory metricHistory = new MetricHistory(collector, new Reporter(), new RefactoringMiner());
-
-            metricHistory.collect(revisionFile, GitRepository.get(repository), "./output.csv");
-        } catch (IOException e) {
-            logger.error("Resource access problem", e);
-        } catch (Exception e) {
-            logger.error("Something went wrong", e);
-        }
-    }
-
-    public void processAncestryCommand(String[] args) {
-        if (args.length == 1) {
-            System.out.println("Usage: metric-history ancestry <revision file> <repository path> <output file> ");
+            System.out.println("Incorrect number of arguments (" + parameters.length + ')');
             System.out.println();
-            System.out.println("<revision file>     is the path to the file containing the revision to analyse.");
-            System.out.println("<repository path>   is the path to the folder containing .git folder");
-            System.out.println("<output file>       is the path of the file where the results will be stored.");
-            return;
+            command.printUsage();
         }
-
-        atLeast(4, args);
-
-        String revisionFile = normalizePath(args[1]);
-        String repository = normalizePath(args[2]);
-        String outputFile = normalizePath(args[3]);
-
-        Ancestry ancestry;
-        try {
-            ancestry = new Ancestry(GitRepository.get(repository), new RefactoringMiner());
-        } catch (IOException e) {
-            throw new IllegalArgumentException("This repository doesn't have version control: " + repository);
-        }
-        ancestry.make(revisionFile);
-
-        try (CSVPrinter writer = ancestry.getPrinter(outputFile)) {
-            ancestry.export(writer);
-        } catch (IOException e) {
-            logger.error("I/O error with file {}", outputFile, e);
-        }
-    }
-
-    public void processConvertCommand(String[] args) {
-        if (args.length == 1) {
-            System.out.println("Usage: metric-history convert <folder> <output file> ");
-            System.out.println();
-            System.out.println("<folder>            is the path of the root folder containing the results from the " +
-                    "third party tool");
-            System.out.println("<output file>       is the path of the file where the results will be stored.");
-            return;
-        }
-        atLeast(3, args);
-
-        String inputFolder = normalizePath(args[1]);
-        String outputFile = normalizePath(args[2]);
-
-        SourceMeterConverter.convert(inputFolder, outputFile);
-    }
-
-    public void processDiffCommand(String[] args) throws IOException {
-        if (args.length == 1) {
-            System.out.println("Usage: metric-history diff <ancestry file> <raw file> <output file>");
-            System.out.println();
-            System.out.println("<ancestry file>     is the path of the file produced by 'ancestry' command");
-            System.out.println("<raw file>          is the path of the file produced by 'convert' command");
-            System.out.println("<output file>       is the path of the file where the results will be stored.");
-            return;
-        }
-        atLeast(4, args);
-
-        String ancestryFile = normalizePath(args[1]);
-        String rawFile = normalizePath(args[2]);
-        String outputFile = normalizePath(args[3]);
-
-        HashMap<String, String> ancestry = Ancestry.load(ancestryFile);
-        if (ancestry.isEmpty()) {
-            return;
-        }
-
-        ClassStore model = Stores.loadClasses(rawFile);
-
-        Difference difference = new Difference();
-        try (CSVPrinter writer = new CSVPrinter(new FileWriter(outputFile), Stores.getFormat())) {
-            difference.export(ancestry, model, writer);
-        } catch (IOException e) {
-            logger.error("I/O error with file {}", outputFile, e);
-        }
-    }
-
-    public void processMongoCommand(String[] args) throws IOException {
-        if (args.length <= 2) {
-            printMongoUsage();
-            return;
-        }
-        atLeast(4, args);
-
-        final String action = args[1];
-        final String file = normalizePath(args[2]);
-        String databaseName = args[3];
-
-        String connectionString = args.length == 5 ? args[4] : null;
-
-        Database database = DatabaseBuilder.build(connectionString, databaseName);
-
-        switch (action) {
-            case "raw":
-                ClassStore raw = Stores.loadClasses(file);
-                database.setRaw(raw);
-                break;
-
-            case "diff":
-                ClassStore diff = Stores.loadClasses(file);
-                database.setDiff(diff);
-                break;
-
-            case "ancestry":
-                HashMap<String, String> ancestry = Ancestry.load(file);
-
-                if (ancestry.isEmpty()) {
-                    logger.warn("No ancestry was found...");
-                    return;
-                }
-
-                database.persist(ancestry);
-                break;
-
-            default:
-                printMongoUsage();
-                break;
-        }
-    }
-
-    private void printMongoUsage() {
-        System.out.println("Usages:");
-        System.out.println("     metric-history mongo raw <raw file> <database name> [remote URI]");
-        System.out.println("     metric-history mongo diff <diff file> <database name> [remote URI]");
-        System.out.println("     metric-history mongo ancestry <ancestry file> <database name> [remote URI]");
-    }
-
-    private String normalizePath(String arg) {
-        return FilenameUtils.normalize(new File(arg).getAbsolutePath());
     }
 
     /**
-     * Verify the number of arguments.
-     *
-     * @param expected the number of arguments expected
-     * @param args     the container of arguments
-     * @throws IllegalArgumentException if the number of arguments doesn't match the actual number of arguments
+     * Short form for <code>commands.put(command.getName(), command)</code>.
+     * @param command the command
+     * @throws IllegalStateException if the command is already present in <code>commands</code>.
      */
-    private void atLeast(int expected, String[] args) {
-        if (expected > args.length) {
-            throw new IllegalArgumentException("Incorrect number of arguments (" + args.length + ") expected " +
-                    expected);
+    private void addCommand(Command command) {
+        if (commands.containsKey(command.getName())) {
+            throw new IllegalStateException("The command '" + command.getName() + "' already exists");
         }
+        commands.put(command.getName(), command);
+    }
+
+    private void printUnknownCommand(String name) {
+        System.out.println(
+                "Unknown command '" + name + "'. Verify the spelling and make sure your command is in lowercase.");
+        System.out.println();
+        printHelp();
+    }
+
+    private void printHelp() {
+        help.execute();
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/ch/thomsch/Command.java
@@ -0,0 +1,363 @@
+package ch.thomsch;
+
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.io.FilenameUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Objects;
+
+import ch.thomsch.converter.SourceMeterConverter;
+import ch.thomsch.csv.Stores;
+import ch.thomsch.database.Database;
+import ch.thomsch.database.DatabaseBuilder;
+import ch.thomsch.export.Reporter;
+import ch.thomsch.loader.RefactoringMiner;
+import ch.thomsch.metric.Collector;
+import ch.thomsch.metric.SourceMeter;
+import ch.thomsch.model.ClassStore;
+import ch.thomsch.versioncontrol.GitRepository;
+
+abstract class Command {
+
+    private static final Logger logger = LoggerFactory.getLogger(Command.class);
+
+    /**
+     * Returns the name of the command. This is also the string to use to call it in command line.
+     * @return the name, in lowercase.
+     */
+    abstract String getName();
+
+    /**
+     * Parses the parameters for the command.
+     * @param parameters the parameters to parse
+     * @return <code>true</code> if the parameters could be parsed, <code>false</code> otherwise.
+     */
+    abstract boolean parse(String[] parameters);
+
+    /**
+     * Executes the command.
+     */
+    abstract void execute();
+
+    /**
+     * Prints the usage of the command on <code>System.out</code>.
+     */
+    abstract void printUsage();
+
+    String normalizePath(String arg) {
+        return FilenameUtils.normalize(new File(arg).getAbsolutePath());
+    }
+
+    public static class Help extends Command {
+        private final Collection<Command> values;
+
+        Help(Collection<Command> values) {
+            this.values = Objects.requireNonNull(values);
+        }
+
+        @Override
+        public String getName() {
+            return "help";
+        }
+
+        @Override
+        public boolean parse(String[] parameters) {
+            return true; // No parameters for this command
+        }
+
+        @Override
+        public void execute() {
+            System.out.println("Usage: metric-history <command> <parameters>...");
+            System.out.println("or metric-history <command> to learn more about a particular command.");
+            System.out.println();
+            System.out.println("Where <command> can be one of ");
+            values.forEach(command -> System.out.println("- " + command.getName()));
+        }
+
+        @Override
+        void printUsage() {
+            execute();
+        }
+    }
+
+    public static class Ancestry extends Command {
+        private String revisionFile;
+        private GitRepository repository;
+        private String outputFile;
+
+        @Override
+        public String getName() {
+            return "ancestry";
+        }
+
+        @Override
+        public boolean parse(String[] parameters) {
+            if (parameters.length < 3) {
+                return false;
+            }
+
+            revisionFile = normalizePath(parameters[0]);
+            try {
+                repository = GitRepository.get(normalizePath(parameters[1]));
+            } catch (IOException e) {
+                System.out.println("This repository doesn't have version control: " + repository.getDirectory());
+            }
+            outputFile = normalizePath(parameters[2]);
+            return true;
+        }
+
+        @Override
+        public void execute() {
+            final ch.thomsch.Ancestry ancestry = new ch.thomsch.Ancestry(repository, new RefactoringMiner());
+            ancestry.make(revisionFile);
+
+            try (CSVPrinter writer = ancestry.getPrinter(outputFile)) {
+                ancestry.export(writer);
+            } catch (IOException e) {
+                logger.error("I/O error with file {}", outputFile, e);
+            }
+        }
+
+        @Override
+        void printUsage() {
+            System.out.println("Usage: metric-history ancestry <revision file> <repository path> <output file> ");
+            System.out.println();
+            System.out.println("<revision file>     is the path to the file containing the revision to analyse.");
+            System.out.println("<repository path>   is the path to the folder containing .git folder");
+            System.out.println("<output file>       is the path of the file where the results will be stored.");
+        }
+    }
+
+    public static class Collect extends Command {
+        private String revisionFile;
+        private String executable;
+        private String project;
+        private String executableOutput;
+        private String projectName;
+        private String repository;
+
+        @Override
+        public String getName() {
+            return "collect";
+        }
+
+        @Override
+        public boolean parse(String[] parameters) {
+            if (parameters.length < 6) {
+                return false;
+            }
+
+            revisionFile = normalizePath(parameters[0]);
+            executable = normalizePath(parameters[1]);
+            project = normalizePath(parameters[2]);
+
+            repository = parameters[3];
+            if (repository.equalsIgnoreCase("same")) {
+                repository = project;
+            } else {
+                repository = normalizePath(repository);
+            }
+
+            executableOutput = normalizePath(parameters[4]);
+            projectName = parameters[5];
+
+            return true;
+        }
+
+        @Override
+        public void execute() {
+            try {
+                final Collector collector = new SourceMeter(executable, executableOutput, projectName, project);
+                final MetricHistory metricHistory = new MetricHistory(collector, new Reporter(), new RefactoringMiner());
+
+                metricHistory.collect(revisionFile, GitRepository.get(repository), "./output.csv");
+            } catch (IOException e) {
+                logger.error("Resource access problem", e);
+            } catch (Exception e) {
+                logger.error("Something went wrong", e);
+            }
+        }
+
+        @Override
+        void printUsage() {
+            System.out.println("Usage: metric-history collect <revision file> <executable path> <project path> " +
+                    "<repository path> <output dir> <project name>");
+            System.out.println();
+            System.out.println("<revision file>     is the path to the file containing the revision to analyse.");
+            System.out.println("<executable path>   is the path to the executable to collect metrics.");
+            System.out.println("<project path>      is the path to the folder containing the source code or the " +
+                    "project.");
+            System.out.println("<repository path>   is the path to the folder containing .git folder. It can also be " +
+                    "set to 'same' if it's the same as <project path>.");
+            System.out.println("<output dir>        is the path to the folder where the results should be extracted.");
+            System.out.println("<project name>      is the name of the project.");
+        }
+    }
+
+    public static class Convert extends Command {
+        private String inputFolder;
+        private String outputFile;
+
+        @Override
+        public String getName() {
+            return "convert";
+        }
+
+        @Override
+        public boolean parse(String[] parameters) {
+            if (parameters.length < 2) {
+                return false;
+            }
+
+            inputFolder = normalizePath(parameters[0]);
+            outputFile = normalizePath(parameters[1]);
+
+            return true;
+        }
+
+        @Override
+        public void execute() {
+            SourceMeterConverter.convert(inputFolder, outputFile);
+        }
+
+        @Override
+        void printUsage() {
+            System.out.println("Usage: metric-history convert <folder> <output file> ");
+            System.out.println();
+            System.out.println("<folder>            is the path of the root folder containing the results from the " +
+                    "third party tool");
+            System.out.println("<output file>       is the path of the file where the results will be stored.");
+        }
+    }
+
+    public static class Difference extends Command {
+        private String ancestryFile;
+        private String rawFile;
+        private String outputFile;
+
+        @Override
+        public String getName() {
+            return "diff";
+        }
+
+        @Override
+        public boolean parse(String[] parameters) {
+            if (parameters.length < 3) {
+                return false;
+            }
+
+            ancestryFile = normalizePath(parameters[0]);
+            rawFile = normalizePath(parameters[1]);
+            outputFile = normalizePath(parameters[2]);
+
+            return true;
+        }
+
+        @Override
+        public void execute() {
+            final HashMap<String, String> ancestry = ch.thomsch.Ancestry.load(ancestryFile);
+            if (ancestry.isEmpty()) {
+                return;
+            }
+
+            ClassStore model = null;
+            try {
+                model = Stores.loadClasses(rawFile);
+            } catch (IOException e) {
+                logger.error("I/O error while reading file {}", rawFile);
+            }
+
+            final ch.thomsch.Difference difference = new ch.thomsch.Difference();
+            try (CSVPrinter writer = new CSVPrinter(new FileWriter(outputFile), Stores.getFormat())) {
+                difference.export(ancestry, model, writer);
+            } catch (IOException e) {
+                logger.error("I/O error with file {}", outputFile, e);
+            }
+        }
+
+        @Override
+        void printUsage() {
+            System.out.println("Usage: metric-history diff <ancestry file> <raw file> <output file>");
+            System.out.println();
+            System.out.println("<ancestry file>     is the path of the file produced by 'ancestry' command");
+            System.out.println("<raw file>          is the path of the file produced by 'convert' command");
+            System.out.println("<output file>       is the path of the file where the results will be stored.");
+        }
+    }
+
+    public static class Mongo extends Command {
+        private String action;
+        private String file;
+        private String databaseName;
+        private String connectionString;
+
+        @Override
+        public String getName() {
+            return "mongo";
+        }
+
+        @Override
+        public boolean parse(String[] parameters) {
+            if (parameters.length < 3) {
+                return false;
+            }
+
+            action = parameters[0];
+            this.file = normalizePath(parameters[1]);
+            databaseName = parameters[2];
+            connectionString = parameters.length == 4 ? parameters[3] : null;
+
+            return true;
+        }
+
+        @Override
+        public void execute() {
+            final Database database = DatabaseBuilder.build(connectionString, databaseName);
+
+            try {
+                switch (action) {
+                    case "raw":
+                        final ClassStore raw = Stores.loadClasses(file);
+                        database.setRaw(raw);
+                        break;
+
+                    case "diff":
+                        final ClassStore diff = Stores.loadClasses(file);
+                        database.setDiff(diff);
+                        break;
+
+                    case "ancestry":
+                        final HashMap<String, String> ancestry = ch.thomsch.Ancestry.load(file);
+
+                        if (ancestry.isEmpty()) {
+                            logger.warn("No ancestry was found...");
+                            return;
+                        }
+
+                        database.persist(ancestry);
+                        break;
+
+                    default:
+                        printUsage();
+                        break;
+                }
+            } catch (IOException e) {
+                logger.error("I/O error with file {}", file, e);
+            }
+        }
+
+        @Override
+        void printUsage() {
+            System.out.println("Usages:");
+            System.out.println("     metric-history mongo raw <raw file> <database name> [remote URI]");
+            System.out.println("     metric-history mongo diff <diff file> <database name> [remote URI]");
+            System.out.println("     metric-history mongo ancestry <ancestry file> <database name> [remote URI]");
+        }
+    }
+}
