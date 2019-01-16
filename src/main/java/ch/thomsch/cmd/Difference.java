d6a0d7e50b14309d19a385e13e140f374bcad935
Thomas Schweizer <thomas.schweizer@umontreal.ca>
2019-01-16 18:48:19 -0500

    Implemented multi-input to multi-output

--- src/main/java/ch/thomsch/cmd/Difference.java
+++ src/main/java/ch/thomsch/cmd/Difference.java
@@ -23,7 +23,7 @@ public class Difference extends Command {
     private static final Logger logger = LoggerFactory.getLogger(Difference.class);
 
     private String ancestryFile;
-    private String rawFile;
+    private String input;
     private String output;
 
     @Override
@@ -38,7 +38,7 @@ public class Difference extends Command {
         }
 
         ancestryFile = normalizePath(parameters[0]);
-        rawFile = normalizePath(parameters[1]);
+        input = normalizePath(parameters[1]);
         output = normalizePath(parameters[2]);
 
         return true;
@@ -51,14 +51,43 @@ public class Difference extends Command {
             return;
         }
 
-        final ClassStore model = new ClassStore();
-        try {
-            Stores.loadClasses(rawFile, model);
-        } catch (IOException e) {
-            logger.error("I/O error while reading file {}", rawFile);
-            return;
-        }
 
+        if(DiskUtils.isFile(input)) {
+            final ClassStore model = new ClassStore();
+            try {
+                Stores.loadClasses(input, model);
+            } catch (IOException e) {
+                logger.error("I/O error while reading file {}", input);
+                return;
+            }
+            export(ancestry, model);
+        } else if (DiskUtils.isFile(output)) {
+            logger.info("Multi-input to output one file is not supported.");
+        } else {
+            final File inputDir = new File(input);
+            final File outputDir = DiskUtils.createDirectory(output);
+            final LinkedList<Map.Entry<String, String>> entries = new LinkedList<>(ancestry.entrySet());
+            entries.forEach(entry -> {
+                final ClassStore model = new ClassStore();
+                final String revision = entry.getKey();
+                final String parent = entry.getValue();
+
+                final HashMap<String, String> map = new HashMap<>();
+                map.put(entry.getKey(), entry.getValue());
+
+                try {
+                    Stores.loadClasses(new File(inputDir, revision + ".csv").getPath(), model);
+                    Stores.loadClasses(new File(inputDir, parent + ".csv").getPath(), model);
+
+                    exportDiff(map, model, new File(outputDir, revision + ".csv"));
+                } catch (IOException e) {
+                    logger.error("Failed to access file", e);
+                }
+            });
+        }
+    }
+
+    private void export(HashMap<String, String> ancestry, ClassStore model) throws IOException {
         if(DiskUtils.isFile(output)) {
             exportDiff(ancestry, model, new File(output));
         } else {
@@ -88,7 +117,7 @@ public class Difference extends Command {
         System.out.println("Usage: metric-history diff <ancestry file> <raw file> <output>");
         System.out.println();
         System.out.println("<ancestry file>     is the path of the file produced by 'ancestry' command");
-        System.out.println("<raw file>          is the path of the file produced by 'convert' command");
+        System.out.println("<input>             is the path of the file or directory produced by 'convert' command");
         System.out.println("<output>            is the path of the file where the results will be stored or a directory that will contain one file per revision");
     }
 }
