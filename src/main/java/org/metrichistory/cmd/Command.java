28185839b0719fdfac9ef13b511963230f9c9fe6
Thomas Schweizer <thomas.schweizer@umontreal.ca>
2019-08-06 17:38:56 -0400

    Fixed normalization of null paths

--- src/main/java/org/metrichistory/cmd/Command.java
+++ src/main/java/org/metrichistory/cmd/Command.java
@@ -5,6 +5,8 @@ import org.metrichistory.storage.DiskUtils;
 abstract class Command implements Runnable {
 
     String normalizePath(String arg) {
+        if(arg == null)
+            return null;
         return DiskUtils.normalizePath(arg);
     }
 }
