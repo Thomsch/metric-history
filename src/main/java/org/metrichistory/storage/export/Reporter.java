6a3b909f47952e27477b9357116f2aeaf8fd7dff
Thomas Schweizer <thms.sch@gmail.com>
2019-10-03 14:47:37 -0400

    Removed unused and duplicate methods

--- src/main/java/org/metrichistory/storage/export/Reporter.java
+++ src/main/java/org/metrichistory/storage/export/Reporter.java
@@ -2,22 +2,14 @@ package org.metrichistory.storage.export;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
-import org.metrichistory.analyzer.ck.DefaultFormatter;
-import org.metrichistory.model.MetricDump;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * Write results in a CSV file.
  */
 public class Reporter {
-    private static final Logger logger = LoggerFactory.getLogger(Reporter.class);
-
     public static final char DELIMITER = ';';
 
     private FileWriter fileWriter;
@@ -50,34 +42,6 @@ public class Reporter {
     public void finish() throws IOException {
         if (fileWriter != null) {
             fileWriter.close();
-        }
-    }
-
-    public void report(String revision, String parent, MetricDump current) {
-        final List<Object[]> lines = DefaultFormatter.format(revision, parent, current);
-        logger.info("Output size {}", lines.size());
-        for (Object[] line : lines) {
-            try {
-                report(line);
-            } catch (IOException e) {
-                logger.error("Couldn't write results for line {}", Arrays.toString(line));
-            }
-        }
-    }
-
-    public void report(String revision, String parent, MetricDump before, MetricDump current) {
-        final List<Object[]> lines = DefaultFormatter.format(revision, parent, current);
-
-        if(before != null) {
-            lines.addAll(DefaultFormatter.format(parent, null, before));
-        }
-
-        for (Object[] line : lines) {
-            try {
-                report(line);
-            } catch (IOException e) {
-                logger.error("Couldn't write results for line {}", Arrays.toString(line));
-            }
         }
     }
 }
